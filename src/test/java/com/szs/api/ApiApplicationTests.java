ec251c56e15eeed6ceae2dbd68c60fa502087a05
ljh468 <ljh468@naver.com>
2023-08-06 15:21:40 +0900

    build/#1 spring gradle init setting

---
+++ src/test/java/com/szs/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
