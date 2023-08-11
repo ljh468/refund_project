9d52b26d3c1d9d1dcae5ffb3d7fca58a12c76641
ljh468 <ljh468@naver.com>
2023-08-11 18:52:02 +0900

    build/#34 readme 파일 수정 & 서버 포트번호 변경

--- szs-api/src/main/java/com/szs/app/domain/input/SignUpInput.java
+++ szs-api/src/main/java/com/szs/app/domain/input/SignUpInput.java
@@ -1,5 +1,6 @@
 package com.szs.app.domain.input;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -12,15 +13,19 @@ import javax.validation.constraints.NotNull;
 public class SignUpInput {
 
   @NotNull
+  @Schema(example = "szsUserId01")
   private final String userId;
 
   @NotNull
+  @Schema(example = "szsPassword01")
   private final String password;
 
   @NotNull
+  @Schema(example = "마징가")
   private final String name;
 
   @NotNull
+  @Schema(example = "880601-2455116")
   private final String regNo;
 
   @Override
